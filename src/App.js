f0a5065afe33da61ea3a8fe83760e997288711d8
Ta-Muna <tamaripilia@gmail.com>
2022-11-16 02:08:59 +0400

    Added GitHub URL

--- src/App.js
+++ src/App.js
@@ -1,5 +1,19 @@
 import "./App.css";
 
 export default function App() {
-  return <div className="App">Weather App</div>;
+  return (
+    <div className="App">
+      <h1>Weather App</h1>
+      <footer>
+        Project was created by Tamari Pilia{" "}
+        <a
+          href="https://github.com/Ta-Muna/react-weather"
+          rel="noreferrer"
+          target="_blank"
+        >
+          is open-sourced on Github
+        </a>
+      </footer>
+    </div>
+  );
 }
