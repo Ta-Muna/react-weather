fde825957f680653007aecc46294570ddc446ace
Ta-Muna <tamaripilia@gmail.com>
2022-11-16 16:28:50 +0400

    imported bootstrap and moved App.js-contends into div className "Container"

--- src/App.js
+++ src/App.js
@@ -3,17 +3,19 @@ import "./App.css";
 export default function App() {
   return (
     <div className="App">
-      <h1>Weather App</h1>
-      <footer>
-        Project was created by Tamari Pilia{" "}
-        <a
-          href="https://github.com/Ta-Muna/react-weather"
-          rel="noreferrer"
-          target="_blank"
-        >
-          is open-sourced on Github
-        </a>
-      </footer>
+      <div className="container">
+        <h1>Weather App</h1>
+        <footer>
+          Project was created by Tamari Pilia{" "}
+          <a
+            href="https://github.com/Ta-Muna/react-weather"
+            rel="noreferrer"
+            target="_blank"
+          >
+            is open-sourced on Github
+          </a>
+        </footer>{" "}
+      </div>
     </div>
   );
 }
