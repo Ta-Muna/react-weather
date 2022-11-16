19bd99e1e20d11752057e74c61cb758809863a4c
Ta-Muna <tamaripilia@gmail.com>
2022-11-16 19:22:04 +0400

    Added a new component "Weather" with styling

---
+++ src/Weather.js
@@ -0,0 +1,30 @@
+import React from "react";
+import "./Weather.css";
+
+export default function Weather() {
+  return (
+    <div className="Weather">
+      <form>
+        <div className="row">
+          <div className="col-9">
+            <input
+              type="search"
+              placeholder="Type the city.."
+              className="form-control"
+            />
+          </div>
+          <div className="col-3">
+            <input type="submit" value="search" className="btn btn-primary" />
+          </div>
+        </div>
+      </form>
+      <h1>Tbilisi</h1>
+      <img
+        src="https://ssl.gstatic.com/onebox/weather/64/cloudy.png"
+        alt="cloudy"
+      />
+      <h2>Sunny</h2>
+      <h3>20°C</h3>
+    </div>
+  );
+}
