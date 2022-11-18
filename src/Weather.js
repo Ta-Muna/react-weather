c96a3554d8f7e65f02d99631dbbf00760f0de741
Ta-Muna <tamaripilia@gmail.com>
2022-11-18 19:27:02 +0400

    changed icon url to check netlify

--- src/Weather.js
+++ src/Weather.js
@@ -12,7 +12,7 @@ export default function Weather(props) {
       temperature: response.data.main.temp,
       description: response.data.weather[0].description,
       date: new Date(response.data.dt * 1000),
-      iconUrl: `http://openweathermap.org/img/wn/${response.data.weather[0].icon}@2x.png`,
+      iconUrl: `http://openweathermap.org/img/wn/01n@2x.png`,
       city: response.data.name,
     });
   }
