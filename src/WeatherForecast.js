1fa377b6a945a7114ffe221372bbfe80038456da
Ta-Muna <tamaripilia@gmail.com>
2022-11-24 14:28:02 +0400

    Added ApiUrl for daily eatherForecast

--- src/WeatherForecast.js
+++ src/WeatherForecast.js
@@ -1,8 +1,19 @@
 import React from "react";
 import WeatherIcon from "./WeatherIcon";
 import "./WeatherForecast.css";
+import axios from "axios";
 
-export default function WeatherForecast() {
+export default function WeatherForecast(props) {
+  function handleResponse(response) {}
+
+  const apiKey = `434de5eada78e8cca4747491478592c5`;
+  let latitude = props.coordinates.lat;
+  let longitude = props.coordinates.lon;
+  const apiUrl = `https://api.openweathermap.org/data/2.5/onecall?lat=${latitude}&lon=${longitude}&appid=${apiKey}$units=metric
+`;
+
+  axios.get(apiUrl).then(handleResponse);
+
   return (
     <div className="WeatherForecast">
       <div className="row">
